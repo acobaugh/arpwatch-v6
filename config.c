9fa23813566f906a4ab6b32fee7fed28029d49b7
Andy Cobaugh <phalenor@gmail.com>
2011-01-12 13:28:46 -0500

    config.c: massive changes. move mysql options to db_mysql.h. use CFG_END(). use HAVE_MYSQL to make mysql options optional

--- config.c
+++ config.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <confuse.h>
-#include "config.h"
+#include <stdlib.h>
+#include <string.h>
 
-int read_config(char *f) {
+#include "config.h"
+#include "main.h"
+
+#ifdef HAVE_MYSQL
+#include "db/db_mysql.h"
+#endif
+
+extern int opt_debug;
+extern network_config_t networks[];
+
+int num_networks = 0;
+
+int config_read(char *f) {
 	char *config_default = "/etc/arpwatch-v6/arpwatch-v6.conf";
-	cfg_t *cfg;
-	int error;
+	cfg_t *cfg, *cfg_network;
 
 	if (f == NULL) {
 		f = config_default;
@@ -16,79 +28,99 @@ int read_config(char *f) {
 		CFG_STR("name", "default", CFGF_NONE),
 		CFG_STR("device", "eth0", CFGF_NONE),
 		CFG_STR("filter", "any", CFGF_NONE),
-		CFG_STR_LIST("databases", "{default}", CFGF_NONE)
+		CFG_STR_LIST("databases", "{default}", CFGF_NONE),
+		CFG_END()
 	};
 
-
-	/* mysql database type options */
-	cfg_opt_t database_mysql_opts[] = {
-		CFG_STR("host", "localhost", CFGF_NONE),
-		CFG_STR("db", "arpwatch", CFGF_NONE),
-		CFG_STR("password", "arpwatch", CFGF_NONE),
-		CFG_STR_LIST("notify_methods", NULL, CFGF_NONE)
-	};
 
 	/* file database type options */
 	cfg_opt_t database_file_opts[] = {
-		CFG_STR("file", "/var/arpwatch-v6/arp.dat", CFGF_NONE)
+		CFG_STR("file", "/var/arpwatch-v6/arp.dat", CFGF_NONE),
+		CFG_END()
 	};
 	
 	/* database and notification section 'type' */
 	cfg_opt_t database_opts[] = { 
-		CFG_SEC("mysql", database_mysql_opts, CFGF_TITLE | CFGF_MULTI),
-		CFG_SEC("file", database_file_opts, CFGF_TITLE | CFGF_MULTI)
+		CFG_SEC("file", database_file_opts, CFGF_NONE),
+#ifdef HAVE_MYSQL
+		CFG_SEC("mysql", database_mysql_opts, CFGF_NONE),
+#endif
+		CFG_END()
 	};
+
 
 	/* email notification method options */
 	cfg_opt_t notify_method_email_opts[] = {
 		CFG_STR("from", NULL, CFGF_NONE),
-		CFG_STR("to", NULL, CFGF_NONE)
+		CFG_STR("to", NULL, CFGF_NONE),
+		CFG_END()
 	};
 
 	/* syslog notification method options */
 	cfg_opt_t notify_method_syslog_opts[] = {
 		CFG_STR("facility", "daemon", CFGF_NONE),
-		CFG_STR("priority", "info", CFGF_NONE)
+		CFG_STR("priority", "info", CFGF_NONE),
+		CFG_END()
 	};
 
+	/* notify methods */
 	cfg_opt_t notify_method_opts[] = {
-		CFG_SEC("email", notify_method_email_opts, CFGF_TITLE | CFGF_MULTI),
-		CFG_SEC("syslog", notify_method_syslog_opts, CFGF_TITLE | CFGF_MULTI)
+		CFG_SEC("email", notify_method_email_opts, CFGF_NONE),
+		CFG_SEC("syslog", notify_method_syslog_opts, CFGF_NONE),
+		CFG_END()
 	};
 
 	/* top-most config file options/sections */
 	cfg_opt_t opts[] = {
 		CFG_SEC("network", network_opts, CFGF_MULTI),
 		CFG_SEC("databases", database_opts, CFGF_NONE),
-		CFG_SEC("notify_methods", notify_method_opts, CFGF_NONE)
+		CFG_SEC("notify_methods", notify_method_opts, CFGF_NONE),
+		CFG_END()
 	};
 
 	/* first try the config file passed into read_config() */
 	cfg = cfg_init(opts, CFGF_NONE);
-	
+
 	switch(cfg_parse(cfg, f)) {
 		case CFG_FILE_ERROR:
-			printf(stderr, "Could not open config file, %s\n", f);
+			fprintf(stderr, "Could not open config file, %s\n", f);
 			cfg_free(cfg);
 			return 0;
 			break;
 		case CFG_PARSE_ERROR:
-			printf(stderr, "Parse error reading config file, %s\n", f);
+			fprintf(stderr, "Parse error reading config file, %s\n", f);
 			cfg_free(cfg);
 			return 0;
 			break;
 		case CFG_SUCCESS:
-			printf("Read config file, %s\n", f);
-			return 1;
+			if (opt_debug) {
+				printf("Read config file, %s\n", f);
+			}
 			break;
 		default:
-			printf(stderr, "Unknown error reading config file, %s\n", f);
+			fprintf(stderr, "Unknown error reading config file, %s\n", f);
 			cfg_free(cfg);
 			return 0;
 			break;
 	}
 
+	num_networks = cfg_size(cfg, "network");
+	
+	/* print networks */
+	int i, j, num_databases;
+	for (i = 0; i < num_networks; i++) {
+		cfg_network = cfg_getnsec(cfg, "network", i);
+		strncpy(networks[i].name, cfg_getstr(cfg_network, "name"), sizeof(networks[i].name));
+		strncpy(networks[i].device, cfg_getstr(cfg_network, "device"), sizeof(networks[i].name));
+		strncpy(networks[i].filter, cfg_getstr(cfg_network, "filter"), sizeof(networks[i].name));
+		num_databases = cfg_size(cfg_network, "databases");
+		networks[i].num_databases = num_databases;
+		for (j = 0; j < num_databases; j++) {
+			strncpy(networks[i].databases[j], cfg_getnstr(cfg_network, "databases", j), sizeof(networks[i].databases[j]));
+		}
+	}
+
 	cfg_free(cfg);
 
-	return 0;
+	return 1;
 }
