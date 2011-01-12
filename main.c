3ead1361fefa77a0048666c9773d75576e56684d
Andy Cobaugh <phalenor@gmail.com>
2011-01-12 13:18:40 -0500

    main.c: debug messages, build filter expression, use config file to get options

--- main.c
+++ main.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <getopt.h>
 
 #include "utils.h"
 #include "config.h"
 #include "capture.h"
+#include "main.h"
+
+int opt_debug = 0;
+network_config_t networks[];
+extern int num_networks;
 
 int main(int argc, char *argv[]) {
-	int c;
+	int c, i, j;
 	char *config_file = NULL;
-	int debug = 0;
+	char *base_filter = "(icmp6 or arp) and ";
 
 	/* read in command line options */
 	while ((c = getopt(argc, argv, "c:d")) != -1) {
 		switch(c) {
 			case 'd':
-				debug = 1;
+				opt_debug = 1;
+				printf("option -d\n");
 				break;
 			case 'c':
 				config_file = optarg;
-				printf("config_file = %s\n", config_file);
-
+				if (opt_debug) {
+					printf("option -c: %s\n", config_file);
+				}
 				break;
 			default:
 				usage();
@@ -27,14 +35,28 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	if (!read_config(config_file)) {
-		printf(stderr, "Failed to read config file\n");
+	if (!config_read(config_file)) {
+		fprintf(stderr, "Failed to read config file\n");
 		die();
 	}
 
-	char dev[] = "eth0";
-	char filter_expr[] = "(icmp6 or arp) and ( (net 128.118.200.0/23) or (net 2610:8:7800:14::/64) ) ";
-	capture(dev, filter_expr);
+	if (opt_debug) printf("Number of networks to monitor: %i\n", num_networks);
+	for (i = 0; i < num_networks; i++) {
+		/* build the filter based on the provided filter and base_filter */
+		char *filter_expr = malloc(strlen(networks[i].filter) + strlen(base_filter) + 2);
+		sprintf(filter_expr, "%s(%s)", base_filter, networks[i].filter);
+		
+		if (opt_debug) {
+			printf("Network: %s (%i)\n", networks[i].name, i);
+			printf("\tdevice = %s\n", networks[i].device);
+			printf("\tfilter = %s\n", filter_expr);
+			printf("\tDatabases:\n");
+			for (j = 0; j < networks[i].num_databases; j++) {
+				printf("\t\t%s\n", networks[i].databases[j]);
+			}
+		}
+		capture(&networks[i].device, filter_expr);
+	}
 
 	return 0;
 }
