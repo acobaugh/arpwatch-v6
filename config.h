34b2eb3dc9e7839e6750b8c623b4ea2dd6f3185b
Andy Cobaugh <phalenor@gmail.com>
2011-01-12 13:21:04 -0500

    config.h: inclusion guard, network_config_t

--- config.h
+++ config.h
@@ -1,1 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H 1
+
 int config_read(char *);
+
+typedef struct network_config {
+	int foo;
+	char name[64];
+	char device[8];
+	char filter[1024];
+	int num_databases;
+	char databases[2][16];
+} network_config_t;
+
+#endif
