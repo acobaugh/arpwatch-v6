284fdf5e26f2339708c12016cdc4275a9ddcbcf0
Andy Cobaugh <phalenor@gmail.com>
2011-03-14 17:31:38 -0400

    define PACKET_TYPE_*, pass args to callback, pass network index to capture

--- capture.h
+++ capture.h
@@ -3,8 +3,17 @@
 
 #include <pcap.h>
 
-void capture(char *, char *filter_expr);
+/* internal packet types */
+#define PACKET_TYPE_IPV6_RS 0
+#define PACKET_TYPE_IPV6_RA 1
+#define PACKET_TYPE_IPV6_NS 2
+#define PACKET_TYPE_IPV6_NA 3
+#define PACKET_TYPE_ARP_REQUEST 4
+#define PACKET_TYPE_ARP_REPLY 5
+#define PACKET_TYPE_RARP_REPLY 6
 
-void callback(u_char *, struct pcap_pkthdr *, u_char *);
+void capture(char *, char *filter_expr, int);
+
+void callback(u_char *, const struct pcap_pkthdr *, const u_char *);
 
 #endif
